4cfeedca2ceb8245d8f4725b5cacd43e9a033986
SMD.SAYEED <96527307+smdsayeed@users.noreply.github.com>
2022-11-06 12:20:19 +0530

    Update arrays.js

--- arrays.js
+++ arrays.js
@@ -17,13 +17,17 @@ console.log(n1)
  const rm = [1,2,3,4]
  rm.pop()  //removes the last element from the array
 let m = rm.pop()
- console.log(rm , m)
-//pop returns the poped element
- 
- 
+ console.log(rm , m)  //pop returns the poped element
  //expected output: [1,2,3,]
 
 
 let a = [1,2,3]
 let b = a.shift() //removes the first element and returns it also modifies the original array
 console.log(b , a)
+// output: 1, [2,3]
+
+let num = [1,2,3,4]
+num.unshift(33) // adds elements to the beginning
+console.log(num)
+
+//output: [33,1,2,3]
