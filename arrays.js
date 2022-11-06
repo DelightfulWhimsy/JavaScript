3005c6568ec2fd53e9d9d30231823eaa760c0279
SMD.SAYEED <96527307+smdsayeed@users.noreply.github.com>
2022-11-06 12:02:49 +0530

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,22 @@
+// Array methods
+
+const n = [1,2,3,]
+n.toString();  // toString() used to convert the array elements into strings
+console.log(n)
+
+//expected ouput: 1,2,3, which has become a string
+
+let n1=[74,5,6]
+n1.join("and")
+console.log(n1)
+
+
+//expected output: 74 and 5 and 6
+//join() joins the array elements with the provided arguments in it.
+
+ const rm = [1,2,3,4]
+ rm.pop()  //removes the last element from the array
+ console.log(rm)
+ 
+ 
+ //expected output: [1,2,3,]
