1dbbdbce24f776224915a363787ef1812a084088
SMD.SAYEED <96527307+smdsayeed@users.noreply.github.com>
2022-11-08 19:47:24 +0530

    Create reduce().js

---
+++ reduce().js
@@ -0,0 +1,11 @@
+
+/reduce()-- reduces the array to an single value.
+here a , b are argumets (arguments = values which returns some output)
+
+let arr = [1,2,3,4]
+let new1 = arr.reduce((a,b)=>{
+  return a+b
+})
+  console.log(new1)
+
+//output 10
