2fe49bfc3e0331b0ee71b053ddf135ec2834f5d4
SMD.SAYEED <96527307+smdsayeed@users.noreply.github.com>
2022-11-10 19:13:15 +0530

    Update reduce().js
    used reduce () to calculate the factorial of the given number, from an array of the first n natural number

--- reduce().js
+++ reduce().js
@@ -9,3 +9,14 @@ let new1 = arr.reduce((a,b)=>{
   console.log(new1)
 
 //output 10
+
+
+// used reduce () to calculate the factorial of the given number, from an array of the first n natural number
+let arr = [1,2,3,4]
+let n= arr.reduce((x1,x2)=>{
+  return x1*x2
+})
+
+console.log(n)
+
+//output: 24
