ed349f56d31545ed6838cdbd1be50a97958aec5f
SMD.SAYEED <96527307+smdsayeed@users.noreply.github.com>
2022-11-12 10:37:31 +0530

    Update map().js

--- map().js
+++ map().js
@@ -2,12 +2,24 @@
 //map ()--- used to create a new array by performing some operations on each array element
 
 const num = [1,23,34,34]
-num.map((value,index,array)=>{
+num.map((value,index,array)=>{ //new array
    console.log(value,index,array)
+ 
  })
+
 
 //expected output
 /* 1 0 [1,23,34,34]
 23 1 [1,23,34,34]
 34 2 [1,23,34,34]
 45 3 [1,23,34,34] */
+
+
+const arr= [1,2,3]
+let a = arr.map((value)=>{
+return value+1
+})
+console.log(a)
+
+//[2,3,4]
+//original array was[1,2,3]
